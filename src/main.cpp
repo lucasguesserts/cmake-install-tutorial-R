df03e7accef5c31d998e85e1133719260e1c28aa
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-10-10 14:52:39 -0300

    build: update dependency 'P' to version 0.3.0

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,22 @@
 
 class ClassR : public P::ClassP {
 public:
-    ClassR() = default;
-    ~ClassR() = default;
-    virtual void print() final {
-        P::ClassP::print();
-        std::cout << "ClassR" << std::endl;
-    }
+  ClassR() = default;
+  ~ClassR() = default;
+  virtual void print() final {
+    nlohmann::json ex1 = nlohmann::json::parse(R"(
+      {
+          "pi": 3.141,
+          "happy": true
+      }
+      )");
+    P::ClassP::print(ex1);
+    std::cout << "ClassR" << std::endl;
+  }
 };
 
 int main() {
-    ClassR r;
-    r.print();
-    return 0;
+  ClassR r;
+  r.print();
+  return 0;
 }
